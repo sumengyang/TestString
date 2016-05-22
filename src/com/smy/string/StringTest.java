ace4ba62477604661c60bb4427caa182e24e1332
sumengyang <842298634@qq.com>
2016-05-23 00:19:54 +0800

    Update StringTest.java

--- src/com/smy/string/StringTest.java
+++ src/com/smy/string/StringTest.java
@@ -6,24 +6,24 @@ public class StringTest {
 		// TODO Auto-generated method stub
 		String str1 = "hello world";
 		String str2 = "hello world";
-		System.out.println(str1 == str2);
-		System.out.println(str1.equals(str2));
+		System.out.println(str1 == str2);  //true
+		System.out.println(str1.equals(str2));//true
 		
 
 		String str = "I like";// ---------1--------
-		System.out.println(System.identityHashCode(str));
+		System.out.println(System.identityHashCode(str));//123
 		str = str + "java";// --------2---------
-		System.out.println(System.identityHashCode(str));
+		System.out.println(System.identityHashCode(str));//反正不是123
 
 		String s = new String();
-		System.out.println(s);
+		System.out.println(s); //空行
 		String s1 = new String("hello");
-		System.out.println(s1);
+		System.out.println(s1);//hello
 		char[] c = { 'h', 'e', 'l', 'l', 'o' };
 		String s2 = new String(c);
-		System.out.println(s2);
+		System.out.println(s2);//hello
 		String s3 = new String(c, 1, 3);
-		System.out.println(s3);
+		System.out.println(s3);//ell
 	}
 
 }
